12f0c9302d5051892be040466a502cbbdd90e2e4
yunjia <yunjia.hao@mail.utoronto.ca>
2024-09-21 16:28:26 -0400

    Add new route so that says hi to user's name

--- hello.py
+++ hello.py
@@ -3,5 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():    
+    return '<h1>Hello World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
