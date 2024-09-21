a14d463bf3b3932f849b5022690c93b167833f0d
yunjia <yunjia.hao@mail.utoronto.ca>
2024-09-21 18:20:05 -0400

    checks if email is uoft address or not

--- hello.py
+++ hello.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 
-@app.route('/')
-def index():    
-    return render_template('user.html', name="World")
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your email?', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        if 'utoronto' in email:
+            session['name'] = name
+            session['email'] = email
+            return render_template('index.html', form=form, name=name, email=email)
+        else:
+            flash('Please use a UofT email address.')
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
