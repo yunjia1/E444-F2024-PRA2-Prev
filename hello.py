dbaabdb9c435b86fe198926e4d4babfcaf5de79f
yunjia <yunjia.hao@mail.utoronto.ca>
2024-09-23 13:24:13 -0400

    files for activity 1_3

--- hello.py
+++ hello.py
@@ -1,12 +1,20 @@
-
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
 @app.route('/')
-def index():    
-    return '<h1>Hello World!</h1>'
+def index():
+    return render_template('index.html', name = 'World',
+                           current_time=datetime.utcnow())
+
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('index.html', name=name, current_time=datetime.utcnow())
