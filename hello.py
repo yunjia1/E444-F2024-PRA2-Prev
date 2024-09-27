f39f1d756a8d80419a4bb037748166627347e99a
yunjia <yunjia.hao@mail.utoronto.ca>
2024-09-27 13:03:32 -0400

    change to use email validator

--- hello.py
+++ hello.py
@@ -2,19 +2,18 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, ValidationError,Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
-
+    
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
-    email = StringField('What is your email address?', validators=[DataRequired(), Email(message="Email must contain an '@'")])
+    email = EmailField('What is your email address?', validators=[DataRequired(), Email(message="Please include an ‘@’ in the email address. '{email}' is missing an ‘@’.")])
     submit = SubmitField('Submit')
 
 
